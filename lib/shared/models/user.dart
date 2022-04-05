4504380daaff93dee89b057ac44f77e0002cb03b
Felipe Araujo <felipe.araujo@nubank.com.br>
2022-04-04 23:27:56 -0300

    Creates a model for User entity

---
+++ lib/shared/models/user.dart
@@ -0,0 +1,24 @@
+class User {
+  int id = 0;
+  String name = '';
+  String email = '';
+  String phone = '';
+
+  User({
+    this.id = 0,
+    this.name = '',
+    this.email = '',
+    this.phone = '',
+  });
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+  }
+
+  listFromJson(json) {
+    return (json as List).map((e) => User.fromJson(e)).toList();
+  }
+}
